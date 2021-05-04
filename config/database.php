9ecf0a8ee9bb44a0828356f1a2f1fbaf744d1621
Sandrii13 <sandra1398DAW@gmail.com>
2021-05-04 18:28:23 +0200

    Procfile de heroku

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'database' => env('DB_DATABASE', 'd1d36r6trllfb1'),
             'username' => env('DB_USERNAME', 'sbdtduniozwuwf'),
             'password' => env('DB_PASSWORD', 'b24962ac2cd6aaf247d3cd75318988853ce4497b00e217d9063021b0bd8a1f4a'),
-            'charset' => 'utf8',
+            'charset' => 'utf8mb4',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
